4e1a5d3c094b9b6ae58a07ce06a87ce511effc85
Nathalia Ramos <nathii.siilva@gmail.com>
2023-11-03 20:46:58 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/nathalia/projetotreinamentojavaminsait/ProjetoTreinamentoJavaMinsaitApplication.java
@@ -0,0 +1,13 @@
+package br.com.nathalia.projetotreinamentojavaminsait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTreinamentoJavaMinsaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTreinamentoJavaMinsaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nathalia/projetotreinamentojavaminsait/ProjetoTreinamentoJavaMinsaitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nathalia.projetotreinamentojavaminsait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTreinamentoJavaMinsaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
